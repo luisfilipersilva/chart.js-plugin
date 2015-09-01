2dd94ea0510281bd082b46a57d7ac5429fa5db9b
Luis Silva <luisfilipersilva@gmail.com>
2015-09-01 15:02:48 -0300

    Create generate-data.php

---
+++ generate-data.php
@@ -0,0 +1,21 @@
+//$operadora -> array with the data of he class 'operadora'
+//$ths->_terminal > class 'terminal' variable
+
+
+foreach ($operadoras as $this->_terminal) {
+			
+			$operadora = $this->_terminal->getOperadora( );
+			$nome_operadora = $this->_terminal->nomeoperadoras( $operadora );
+			$cor = $this->_terminal->color( $nome_operadora );
+			
+			if($cor == '')
+			{
+				$cor = 'FFFFFF';
+			}
+			
+// this codde generate the javascipt variable based on the '$operadora' data			
+			$val.=  "{";
+			$val.="value:".$this->_terminal->getParticipacao().",";
+			$val.="label:'".$this->_terminal->getOperadora()."',";
+			$val.="color:'#".$cor."'},";
+		}
